aaa66cbc7da98d5b07f4b6ec360615d3b1c2c7a9
panutat-p <pp.panutat@gmail.com>
2022-06-16 21:05:41 +0700

    install dev packages, lint all

--- src/handlers/hello.js
+++ src/handlers/hello.js
@@ -5,16 +5,16 @@ const client = new S3Client({ region: 'ap-southeast-1' });
 const command = new ListBucketsCommand({});
 
 export async function handler() {
-    // await new Promise((r) => setTimeout(r, 2000));
-    try {
-        const response = await client.send(command);
-        console.info(`Total: ${response.Buckets?.length}`);
-        return {
-            statusCode: 200,
-            body: response.Buckets,
-        }
-    } catch (e) {
-        console.log(e);
-        throw e;
-    }
+  // await new Promise((r) => setTimeout(r, 2000));
+  try {
+    const response = await client.send(command);
+    console.info(`Total: ${response.Buckets?.length}`);
+    return {
+      statusCode: 200,
+      body: response.Buckets,
+    };
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
 }
